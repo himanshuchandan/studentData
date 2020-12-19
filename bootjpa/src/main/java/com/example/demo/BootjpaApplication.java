f1ad9b0fd5c568bccbb46cd915940feced9a673f
HI20170482 <HI20170482@APL-5CD0177C35>
2020-12-19 22:33:08 +0530

    added war packaging

--- bootjpa/src/main/java/com/example/demo/BootjpaApplication.java
+++ bootjpa/src/main/java/com/example/demo/BootjpaApplication.java
@@ -2,12 +2,21 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BootjpaApplication {
+public class BootjpaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootjpaApplication.class, args);
+		//SpringApplication.run(BootjpaApplication.class, args);
 	}
+	
+	//newly added
+	@Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	  return application.sources(BootjpaApplication.class);
+	 }
 
 }
